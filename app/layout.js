76173ebdd8a931a9a298808682d2c8c691322146
Ze0ro99 <146000493+Ze0ro99@users.noreply.github.com>
2025-05-12 12:21:35 +0000

    Pending changes exported from your codespace

---
+++ app/layout.js
@@ -0,0 +1,1 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"; export default function RootLayout({ children }) { return (<html lang="en"><body>{children}<SpeedInsights /></body></html>); }
